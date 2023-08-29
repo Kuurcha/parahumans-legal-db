d2b56e7b4569018385eaac2319deca314fa77029
nking <nking07049925@gmail.com>
2023-08-29 22:29:07 +0300

    <></>

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import React, { useState as useSus } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
 function App() {
   const ඞ = "span";
   const [amogus, sus] = useSus(1);
 
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route path="" element={<Link to="/amogus">enter sus loop</Link>} />
+            <Route path="amogus" element={<Link to="/sus">ඞ</Link>} />
+            <Route path="sus" element={<Link to="/amogus">sus</Link>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
