a402b5b95db573330f91c639a60b045f93f1cf1a
Kurcha <ponykurcha@gmail.com>
2023-08-29 22:20:18 +0300

    barelly working exaple

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import AppRouter from './Components/Router';
+import Page1 from './pages/app1/App1';
+import Page2 from './pages/app2/App2';
+import Root from './App';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>React Router</h1>
+            <nav>
+                <Link to="page1">page1</Link>
+                <Link to="page2">page2</Link>
+            </nav>
+            <Routes>
+                <Route path="page1" element={<Page1 />} />
+                <Route path="page2" element={<Page2 />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Router.tsx
@@ -0,0 +1,18 @@
+import { Router, Link, Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Routes } from 'react-router-dom';
+import React from 'react';
+import Page1 from '../pages/app1/App1';
+import Page2 from '../pages/app2/App2';
+import Root from '../App';
+
+function AppRouter() {
+    return (
+        <Routes>
+            <Route path="/" element={<Root />}>
+                <Route path="page1" element={<Page1 />} />
+                <Route path="page2" element={<Page2 />} />
+            </Route>
+        </Routes>
+    );
+}
+
+export default AppRouter;
