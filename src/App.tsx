82baecb355a54f1aa9b55a5b78bd56bd67540231
Kurcha <ponykurcha@gmail.com>
2023-08-29 23:01:14 +0300

    Simplest router implementation

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,16 @@ import { Link, Route, Routes } from 'react-router-dom';
 import AppRouter from './Components/Router';
 import Page1 from './pages/app1/App1';
 import Page2 from './pages/app2/App2';
-import Root from './App';
+import Home from './pages/home/home';
 function App() {
     return (
         <div className="App">
-            <h1>React Router</h1>
-            <nav>
-                <Link to="page1">page1</Link>
-                <Link to="page2">page2</Link>
-            </nav>
             <Routes>
-                <Route path="page1" element={<Page1 />} />
-                <Route path="page2" element={<Page2 />} />
+                <Route path="/">
+                    <Route path="" element={<Home />}></Route>
+                    <Route path="page1" element={<Page1 />} />
+                    <Route path="page2" element={<Page2 />} />
+                </Route>
             </Routes>
         </div>
     );
---
+++ src/pages/home/home.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from './home';
+
+test('renders learn react link', () => {
+    render(<Home />);
+
+    expect(true).toBe(true);
+});
