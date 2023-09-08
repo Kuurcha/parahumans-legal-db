8128eb8f610e603660962e75f952a1038459780a
Kurcha <ponykurcha@gmail.com>
2023-09-08 21:53:16 +0300

    First working unit test

--- src/Components/ThemePicker/ThemePicker.test.tsx
+++ src/Components/ThemePicker/ThemePicker.test.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import { getByTestId, render, screen } from "@testing-library/react";
 import ThemePicker from "./ThemePicker";
+import { themes } from "../../misc/themes";
 
 describe("Theme picker component", () => {
-  it("should render with themes", () => {
+  it("should render with themes with equal count and content to theme list", () => {
     const themePickerComponent = render(<ThemePicker />);
     const select = themePickerComponent.getByTestId("select-сontainer");
+    const selectChilren = select.querySelectorAll("option");
 
-    console.log("сhildren: " + select.children);
+    expect(selectChilren.length).toBe(themes.length);
+    selectChilren.forEach((option) => {
+      expect(themes).toContain(option.innerHTML);
+    });
   });
 });
