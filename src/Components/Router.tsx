a4978da2388f094b57ac1739ed6e5f27fd53b465
Kurcha <ponykurcha@gmail.com>
2023-09-09 21:59:28 +0300

    Test sketch, collection for the router changed to be exported to sidebar

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -1,25 +1,40 @@
-import { Router, Link, Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Routes } from 'react-router-dom';
-import React from 'react';
-import Page1 from '../pages/app1/App1';
-import Page2 from '../pages/app2/App2';
-import Root from '../App';
+import { Router, Link, Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Routes } from "react-router-dom";
+import React from "react";
+import Page1 from "../pages/app1/App1";
+import Page2 from "../pages/app2/App2";
+import Root from "../App";
 
 type MenuItem = {
-    title: string;
-    href: string;
+  title: string;
+  href: string;
 };
+interface RouterItem extends MenuItem {
+  redirectElement: JSX.Element;
+}
 
+const sidebarPath: RouterItem[] = [
+  { title: "Home", href: "/", redirectElement: <Root /> },
+  { title: "page1", href: "/page1", redirectElement: <Page1 /> },
+  { title: "page2", href: "/page2", redirectElement: <Page2 /> },
+];
 
-
+export const sidebarItems: MenuItem[] = sidebarPath.map(({ title, href }) => ({ title, href }));
 function AppRouter() {
-    return (
-        <Routes>
-            <Route path="/" element={<Root />}>
+  return (
+    <Routes>
+      {sidebarPath.map((item, index) => (
+        <Route key={index} path={item.href} element={item.redirectElement} />
+      ))}
+
+      {/* {sidebarPath.map((item, index) => {
+        <Route key={index} path={item.href} exact render={() => <item.redirectElement />} />;
+      })}
+      {/* <Route path="/" element={<Root />}>
                 <Route path="page1" element={<Page1 />} />
                 <Route path="page2" element={<Page2 />} />
-            </Route>
-        </Routes>
-    );
+            </Route> */}
+    </Routes>
+  );
 }
 
 export default AppRouter;
---
+++ src/Components/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,6 @@
+import { fireEvent, render } from "@testing-library/react";
+describe("Sidebar component should", () => {
+  it("Show all sidebar routes", () => {});
+
+  it("Invoke redirect on sidebar button click", () => {});
+});
